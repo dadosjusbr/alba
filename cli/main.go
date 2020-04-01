6a227a789ccc216aabb3035e05205bc3d2e30fdf
lorenaps <lorena.santpe@gmail.com>
2020-04-01 15:02:30 -0300

    Renomeando arquivo

---
+++ cli/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/dadosjusbr/alba/cli/collector"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Alba"
+	app.Usage = "A tool for manage the process of continuous data release through steps such as: collection, validation, packaging and storage."
+	app.Commands = []*cli.Command{collector.AddCommand()}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
