7692b35ff281dd0b8927a3108339711d83e1457c
lorenaps <lorena.santpe@gmail.com>
2020-04-03 20:48:16 -0300

    Criando endpoints da api

--- web_interface/main.go
+++ web_interface/main.go
@@ -1,16 +1,73 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/dadosjusbr/alba/storage"
+	"github.com/labstack/echo/v4"
 )
 
-func main() {
-	http.HandleFunc("/", index)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+//Message represents a information for the user
+type Message struct {
+	Message string `json:"message"`
 }
 
-func index(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "Hi Alba")
+func main() {
+	e := echo.New()
+
+	e.GET("/alba", index)
+	e.GET("/alba/api/coletores", viewAllCollectors)
+	e.GET("/alba/api/coletores/:id", viewCollectorByID)
+	e.GET("/alba/api/coletores/path", viewCollectorByPath)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+//e.GET("/alba", index)
+func index(c echo.Context) error {
+	return c.String(http.StatusOK, "HTML com lista dos coletores")
+}
+
+//	e.GET("/alba/api/coletores", viewAllCollectors)
+func viewAllCollectors(c echo.Context) error {
+	result, err := storage.GetCollectors()
+	if err != nil {
+		return echo.ErrInternalServerError
+	}
+	if result == nil {
+		msg := Message{"Not found"}
+		return c.JSON(http.StatusOK, msg)
+	}
+
+	return c.JSONBlob(http.StatusOK, result)
+}
+
+//	e.GET("/alba/api/coletores/:id", viewCollectorByID)
+func viewCollectorByID(c echo.Context) error {
+	id := c.Param("id")
+	result, err := storage.GetCollectorByID(id)
+	if err != nil {
+		return echo.ErrInternalServerError
+	}
+	if result == nil {
+		msg := Message{"Not found"}
+		return c.JSON(http.StatusOK, msg)
+	}
+
+	return c.JSONBlob(http.StatusOK, result)
+}
+
+//e.GET("/alba/api/coletores/path", viewCollectorByPath)
+func viewCollectorByPath(c echo.Context) error {
+	path := c.QueryParam("path")
+	result, err := storage.GetCollectorByPath(path)
+	if err != nil {
+		return echo.ErrInternalServerError
+	}
+	if result == nil {
+		msg := Message{"Not found"}
+		return c.JSON(http.StatusOK, msg)
+	}
+
+	return c.JSONBlob(http.StatusOK, result)
 }
