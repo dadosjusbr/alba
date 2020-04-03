6d118cc1967062d85b629187eec804efa282d689
lorenaps <lorena.santpe@gmail.com>
2020-04-03 20:49:40 -0300

    Iniciando arquivo de testes

---
+++ web_interface/test_main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func testViewAllCollectors(t *testing.T) {
+
+}
+
+func testViewCollectorByID(t *testing.T) {
+
+}
+
+func testViewCollectorByPath(t *testing.T) {
+
+}
