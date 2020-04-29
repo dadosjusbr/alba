1452a348553a178becf6d2420ffdc80fda2a545d
lorenaps <lorena.santpe@gmail.com>
2020-04-29 14:06:01 -0300

    Ajuste na nomenclatura

--- ui/api/api.go
+++ ui/api/api.go
@@ -12,16 +12,16 @@ type getterCollector interface {
 	GetCollectors() ([]storage.Collector, error)
 }
 
-type prodGetCollectors struct {
+type prodGetCollector struct {
 }
 
-func (g prodGetCollectors) GetCollectors() ([]storage.Collector, error) {
+func (prod prodGetCollector) GetCollectors() ([]storage.Collector, error) {
 	return storage.GetCollectors()
 }
 
-//GetCollectors set get collectors prod
-func GetCollectors(c echo.Context) error {
-	return getCollectors(c, prodGetCollectors{})
+//AddGetCollector set prodGetCollector
+func AddGetCollector(c echo.Context) error {
+	return getCollector(c, prodGetCollector{})
 }
 
 //NotFound information for the user when their search has no results
@@ -46,8 +46,7 @@ type ExecutionDetails struct {
 	Executions []Execution
 }
 
-//e.GET("/alba/api/coletores", GetCollectors)
-func getCollectors(c echo.Context, getter getterCollector) error {
+func getCollector(c echo.Context, getter getterCollector) error {
 	results, err := getter.GetCollectors()
 	if err != nil {
 		c.Logger().Error(err)
@@ -60,8 +59,8 @@ func getCollectors(c echo.Context, getter getterCollector) error {
 	return c.JSON(http.StatusOK, results)
 }
 
-//ExecutionsByID returns all executions by collector ID. e.GET("/alba/api/coletores/execucoes/:id", ExecutionsByID)
-func ExecutionsByID(c echo.Context) error {
+//ExecutionByID returns all executions by collector ID. e.GET("/alba/api/coletores/execucoes/:id", ExecutionsByID)
+func ExecutionByID(c echo.Context) error {
 	//Mockup
 	data := ExecutionDetails{
 		Entity: "Nome do órgão",
