87ddd0a9b4d90b67901dc7095cd2bdd5a0d12178
lorenaps <lorena.santpe@gmail.com>
2020-09-03 15:51:18 -0300

    Alterando descriçao da cli

--- cli/main.go
+++ cli/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/dadosjusbr/alba/cli/collector"
+	"github.com/dadosjusbr/alba/cli/pipeline"
 	"github.com/dadosjusbr/alba/storage"
 
 	"github.com/urfave/cli/v2"
@@ -31,8 +31,8 @@ func main() {
 
 	app := cli.NewApp()
 	app.Name = "Alba"
-	app.Usage = "A tool for manage the process of continuous data release through steps such as: collection, validation, packaging and storage."
-	app.Commands = []*cli.Command{collector.NewAddCommand(client)}
+	app.Usage = "A tool for manage the process of continuous data release through configurable pipelines runs."
+	app.Commands = []*cli.Command{pipeline.NewAddCommand(client)}
 	err = app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
