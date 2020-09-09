72a3d44cd171f1d3141abae8e01e9aeac605028f
lorenaps <lorena.santpe@gmail.com>
2020-09-08 23:36:42 -0300

    Pegando o commit mais recente do repositorio

--- worker/manager.go
+++ worker/manager.go
@@ -6,30 +6,48 @@ import (
 	"os"
 
 	"github.com/go-git/go-git/v5"
+
+	"github.com/dadosjusbr/alba/storage"
 )
 
 // CloneRepository is responsible for get the latest code version of pipeline repository.
-// From the environment variable BASEDIR creates and returns the DefaultBaseDir for the pipeline.
-func CloneRepository(repo string) (string, error) {
+// From the environment variable BASEDIR creates and returns the DefaultBaseDir for the pipeline
+// and the latest commit in the repository.
+func CloneRepository(repo string) (string, string, error) {
 	baseDir := os.Getenv("BASEDIR")
 	if baseDir == "" {
-		return "", fmt.Errorf("error cloning the repository. BASEDIR env var can not be empty")
+		return "", "", fmt.Errorf("error cloning the repository. BASEDIR env var can not be empty")
 	}
 
 	defaultBaseDir := fmt.Sprintf("%s/%s", baseDir, repo)
 	if err := os.RemoveAll(defaultBaseDir); err != nil {
-		return "", fmt.Errorf("error cloning the repository. error removing previous directory: %q", err)
+		return "", "", fmt.Errorf("error cloning the repository. error removing previous directory: %q", err)
 	}
 
 	url := fmt.Sprintf("https://%s", repo)
 	log.Printf("Cloning the repository [%s] into [%s]\n", url, defaultBaseDir)
-	_, err := git.PlainClone(defaultBaseDir, false, &git.CloneOptions{
+	r, err := git.PlainClone(defaultBaseDir, false, &git.CloneOptions{
 		URL:      url,
 		Progress: os.Stdout,
 	})
 	if err != nil {
-		return "", fmt.Errorf("error cloning the repository: %q", err)
+		return "", "", fmt.Errorf("error cloning the repository: %q", err)
 	}
 
-	return defaultBaseDir, nil
+	ref, err := r.Head()
+	if err != nil {
+		return "", "", fmt.Errorf("error cloning the repository. error getting the HEAD reference of the repository: %q", err)
+	}
+
+	commit, err := r.CommitObject(ref.Hash())
+	if err != nil {
+		return "", "", fmt.Errorf("error cloning the repository. error getting the lattest commit of the repository: %q", err)
+	}
+	return defaultBaseDir, fmt.Sprintf("%s", commit.Hash), nil
+}
+
+// ConfigureDadosjusBR TODO: Função que reúne as regras de negócio para um pipeline DadosJusBR,
+// como configuração das variáveis commit, mes e ano
+func ConfigureDadosjusBR(p storage.Pipeline, month, year string) (storage.Pipeline, error) {
+	return storage.Pipeline{}, nil
 }
