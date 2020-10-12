d1d6c72514658895d0cf34704f7c27a9c34b9ba6
lorenaps <lorena.santpe@gmail.com>
2020-10-12 17:47:35 -0300

    Removendo view

--- ui/server.go
+++ ui/server.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"html/template"
-	"io"
 	"log"
 	"os"
 
@@ -10,16 +8,6 @@ import (
 
 	"github.com/labstack/echo/v4"
 )
-
-// Template represents the html/template.
-type Template struct {
-	templates *template.Template
-}
-
-// Render implements echo Renderer.
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
 
 type application struct {
 	app *echo.Echo
@@ -38,27 +26,11 @@ const (
 	apiPipelineByID = "/alba/api/v1/pipeline/:id"
 	apiRuns         = "/alba/api/v1/runs"
 	apiRunsByid     = "/alba/api/v1/runs/:id"
-	home            = "/alba"
-	runs            = "/alba/:id"
 )
 
 func newApp(dbClient *storage.DBClient) *application {
 	app := echo.New()
 
-	templates := &Template{
-		templates: template.Must(template.ParseGlob("templates/*.html")),
-	}
-
-	app.Static("/static", "templates/assets")
-
-	app.Renderer = templates
-
-	app.GET(home, func(c echo.Context) error {
-		return index(dbClient, c)
-	})
-	app.GET(runs, func(c echo.Context) error {
-		return viewExecutions(dbClient, c)
-	})
 	app.GET(apiPipelines, func(c echo.Context) error {
 		return getPipelines(dbClient, c)
 	})
